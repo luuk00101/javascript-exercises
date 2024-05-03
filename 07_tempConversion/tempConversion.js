71cfcac113b7f6f4ff91995f3994210d6642faf4
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-05-03 11:28:06 +0200

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  // the formula for fahrenheit -> celcius
+  let celsius = (fahrenheit - 32) * (5 / 9);
+
+  // round the number to one decimal
+  celsius = Math.round(celsius * 10) / 10;
+
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  // the formula for celsius -> fahrenheit
+  let fahrenheit = celsius * (9 / 5) + 32;
+
+  // round the number to one decimal
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+
+  return fahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
