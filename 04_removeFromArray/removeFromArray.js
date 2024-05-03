2a76e0c4b094564675af1b3b907a5919acf7fa8e
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-05-03 10:48:15 +0200

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elementsToRemove) {
+  let newArray = [];
 
+  for (const element of array) {
+    if (elementsToRemove.includes(element)) {
+      continue;
+    }
+    newArray.push(element);
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
