9f7725f69f804e56c83a74a10bbb925fdbf11f8f
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-05-03 10:47:23 +0200

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, numberOfTimes) {
+  if (numberOfTimes < 0) return "ERROR";
 
+  let newString = "";
+
+  for (let i = 0; i < numberOfTimes; i++) {
+    newString += string;
+  }
+
+  return newString;
 };
 
 // Do not edit below this line
