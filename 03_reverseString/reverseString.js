25e9efbd9116e6e37b6f0b53a168e4a92068f9a9
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-05-03 10:47:45 +0200

    Solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (inputString) {
+  const stringArray = inputString.split("");
 
+  let newString = "";
+
+  for (i = stringArray.length - 1; i >= 0; i--) {
+    newString += stringArray[i];
+  }
+
+  return newString;
 };
 
 // Do not edit below this line
